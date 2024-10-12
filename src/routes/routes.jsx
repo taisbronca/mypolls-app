2d534a0276beeacb819593d58b7d08626fd73efe
Tais Cristina Frata Bronca <taisbronca@gmail.com>
2024-10-12 20:43:49 -0300

    feat: create routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   );
